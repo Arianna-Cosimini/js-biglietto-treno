073695fc4c7abbfa03aa804f19ef5c61689fe3d0
arianna-cosimini <arianna.cosimini@gmail.com>
2024-01-26 15:24:23 +0100

    Inserito prezzo del biglietto, prezzo per minorenni e per over65

---
+++ js/sript.js
@@ -0,0 +1,42 @@
+
+
+
+
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+
+
+
+const kmToTravel = prompt ('Quanti chilometri desideri percorrere?')
+
+// const age = prompt ('Quanti anni hai?')
+
+const priceKm = 0.21;
+
+const priceTicket = kmToTravel * 0.21;
+console.log(priceTicket);
+
+// prezzo minorenni
+
+const percentageMinors = 20;
+const discountMinors = ((percentageMinors/100)* priceTicket);
+const priceMinors = (priceTicket - discountMinors).toFixed(2);
+console.log('Il prezzo scontato per minoreenni è:€'+ priceMinors);
+
+// fine prezzo minorenni
+
+// prezzo over 65
+
+const percentageOver65 = 40;
+const discountOver65 = ((percentageOver65/100)* priceTicket);
+const priceOver65 = (priceTicket - discountOver65).toFixed(2);
+console.log('Il prezzo scontato per over 65 è:€' + priceOver65);
+
+
+// fine prezzo over 65
