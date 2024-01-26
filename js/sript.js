44d79720d80bdbf8a26a6003e91cb3983101365d
arianna-cosimini <arianna.cosimini@gmail.com>
2024-01-26 17:17:37 +0100

    inizio bonus

--- js/sript.js
+++ js/sript.js
@@ -20,7 +20,7 @@ const age = Number (prompt ('Quanti anni hai?'));
 const priceKm = 0.21;
 
 const priceTicket = kmToTravel * 0.21;
-console.log(priceTicket);
+console.log('Il prezzo per gli utenti di età compresa tra 18 e 65 è:'+ priceTicket);
 
 // prezzo minorenni
 
@@ -41,11 +41,47 @@ console.log('Il prezzo scontato per over 65 è:€' + priceOver65);
 
 // fine prezzo over 65
 
-if (age>17<66){
+// inizio condizione
+
+if (age > 18 && age < 66){
     document.getElementById('price').innerHTML='Il prezzo del tuo biglietto é: <br> €'+ priceTicket;
+    console.log("L'utente ha un'età compresa tra 18 e 65 anni")
 } else if (age < 19){
     document.getElementById('price').innerHTML='Il prezzo del biglietto per te che hai meno di 18 anni é: <br> €'+ priceMinors;
+    console.log("L'utente è minorenne")
+
 } else if (age > 65){
     document.getElementById('price').innerHTML='Il prezzo del biglietto per te che hai più di 65 anni é: <br> €'+ priceOver65;
+    console.log("L'utente ha più di 65 anni")
 
 }
+
+// fine condizione
+
+// bonus
+
+// if (age < 0  age >100){
+//     alert('Dati inseriti non corretti')
+//     console.log("L'utente ha un'età inferiore a 0 o superiore a 100")
+// }
+
+
+    if (!isNaN(age) && age >0){
+
+        console.log('Hai inseito un numero')
+
+    if (!isNaN(kmToTravel)&& kmToTravel >0){
+        console.log('hai inserito un numero')
+    }
+
+    } else {
+        alert('inserisci solo valore numerico')
+        console.log('Solo valore numerico')
+    }
+
+   
+
+
+// fine bonus
+
+
